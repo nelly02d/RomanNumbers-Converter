527dfff78ac39a9afd7eaf203c3bd966ffeede07
Arnel <nelly02d@gmail.com>
2023-03-04 12:05:29 -0500

    add a for loop and while loop to iterate and find the matched value on the number and roman object

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const button = document.getElementById('convert'); // button id in HTML
+const romanLet = document.getElementById('romanOutput'); //roman input
+const inputNumber = document.getElementById('inputNumber'); //number input
+
+//Number and ROman object array
+const convertToRoman = (num) => {
+  const romanAndNum = [
+    {number: 1000, roman: 'M'},
+    {number: 900, roman: 'CM'},
+    {number: 500, roman: 'D'},
+    {number: 400, roman: 'CD'},
+    {number: 100, roman: 'C'},
+    {number: 90,  roman: 'XC'},
+    {number: 50,  roman: 'L'},
+    {number: 40,  roman: 'XL'},
+    {number: 10,  roman: 'X'},
+    {number: 9,   roman: 'IX'},
+    {number: 5,   roman: 'V'},
+    {number: 4,   roman: 'IV'},
+    {number: 1,   roman: 'I'},
+  ];
+
+  let result = '';
+
+  //iterate romanAndNum array object
+  for (let i = 0; i < romanAndNum.length; i++) {
+    //while number is lessthan or equal to number input
+    while (romanAndNum[i].number <= num) {
+    //inputNumber  -  number
+     num -= romanAndNum[i].number;
+      //''   +   romanAndNumber roman
+     result += romanAndNum[i].roman;     
+    }  
+  };
+};
